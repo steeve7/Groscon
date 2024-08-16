aa84d350a2fd987d6581380047816736d87760bd
ofoegbu stephen <stephenchidindu12@gmail.com>
2024-08-16 17:55:29 +0100

    done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  distDir: "build",
+// next.config.js
+module.exports = {
+  reactStrictMode: true,
+  // Any other custom Next.js configurations go here
 };
-
-module.exports = nextConfig;
