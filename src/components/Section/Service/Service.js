4b31594da6c5ec13fef6f9ea9a22528e4fec2247
ofoegbu stephen <stephenchidindu12@gmail.com>
2024-08-17 11:20:16 +0100

    done

--- src/components/Section/Service/Service.js
+++ src/components/Section/Service/Service.js
@@ -1,9 +1,11 @@
+ 'use client'
 import React from 'react'
-import icon from '../Assets/images/Icon.png'
-import icon2 from '../Assets/images/Icon (1).png'
-import icon3 from '../Assets/images/Icon (2).png'
+import icon from '../Assets/images/icon.png'
+import icon2 from '../Assets/images/icon (1).png'
+import icon3 from '../Assets/images/icon (2).png'
 import Subtitle from '@/components/Reuse/Subtitle';
 import Image from 'next/image';
+import {motion} from 'framer-motion'
 
 export default function Service() {
     const items = [
@@ -24,7 +26,7 @@ export default function Service() {
       }
     ];
   return (
-    <div className="md:px-16 px-7 py-8 bg-custom-gray">
+    <div className="md:px-16 px-7 py-8 bg-custom-gray" id="benefits">
       <div>
         <Subtitle
           title="We are Your Grocery Companion"
@@ -33,10 +35,19 @@ export default function Service() {
       </div>
       <div className="w-full flex md:flex-row flex-col justify-center items-center mt-8 gap-6">
         {items.map((item, i) => (
-          <div
+          <motion.div
             key={i}
             className="flex justify-center text-center items-center flex-col gap-4 md:w-[27%] w-full md:h-[400px] lg:h-[230px] py-10 px-10 mx-2 rounded-lg"
             style={{ background: "rgba(230, 247, 230, 1)" }}
+            initial={{ y: 100, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{
+              delay: 0.2,
+              y: { type: "spring", stiffness: 60 },
+              opacity: { duration: 0.2 },
+              ease: "easeIn",
+              duration: 1,
+            }}
           >
             <Image
               src={item.icons}
@@ -52,7 +63,7 @@ export default function Service() {
             >
               {item.desc}
             </p>
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
