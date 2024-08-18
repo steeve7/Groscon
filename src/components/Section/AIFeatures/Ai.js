1c72d1c832216b3ac7f31b6ee80b13e666f065f7
ofoegbu stephen <stephenchidindu12@gmail.com>
2024-08-18 18:31:06 +0100

    done

--- src/components/Section/AIFeatures/Ai.js
+++ src/components/Section/AIFeatures/Ai.js
@@ -1,9 +1,11 @@
+'use client'
 import React from "react";
 import Image from "next/image";
 import Features5 from "../Assets/images/iphone.png";
 import Features6 from "../Assets/images/features5.png";
 import Features4 from "../Assets/images/Features6.png";
 import { IoIosCheckmarkCircleOutline } from "react-icons/io";
+import {motion} from 'framer-motion'
 
 export default function Ai() {
   const lists = [
@@ -31,15 +33,45 @@ export default function Ai() {
         style={{ background: "rgba(241, 141, 61, 1)" }}
       >
         <div className="flex flex-col justify-center lg:items-start items-center md:w-1/2 w-full gap-4">
-          <h2 className="font-semibold lg:text-[48px] md:text-[30px] text-[24px] w-full font-[Euclid Circular A] text-white">
+          <motion.h2
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{
+              delay: 0.2,
+              x: { type: "spring", stiffness: 60 },
+              opacity: { duration: 1 },
+              ease: "easeIn",
+            }}
+            className="font-semibold lg:text-[48px] md:text-[30px] text-[24px] w-full font-[Euclid Circular A] text-white"
+          >
             Do More with Our AI-powered Features
-          </h2>
-          <p className="w-full font-[Euclid Circular A] text-start md:text-[18px] text-[16px] text-white">
+          </motion.h2>
+          <motion.p
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{
+              delay: 0.4,
+              x: { type: "spring", stiffness: 60 },
+              opacity: { duration: 1 },
+              ease: "easeIn",
+            }}
+            className="w-full font-[Euclid Circular A] text-start md:text-[18px] text-[16px] text-white"
+          >
             Chat with our AI assistant, discover healthier recipes and generate
             recipes using leftover groceries for a sustainable meal plan.
-          </p>
+          </motion.p>
         </div>
-        <div className="lg:mt-0 mt-10">
+        <motion.div
+          className="lg:mt-0 mt-10"
+          initial={{ x: 100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{
+            delay: 0.2,
+            x: { type: "spring", stiffness: 60 },
+            opacity: { duration: 1 },
+            ease: "easeIn",
+          }}
+        >
           <Image
             src={Features5}
             alt="features_image"
@@ -50,30 +82,61 @@ export default function Ai() {
             alt="features_image"
             className="md:w-[343px] md:hidden flex"
           />
-        </div>
+        </motion.div>
       </div>
       <div className="flex md:flex-row flex-col justify-around items-center gap-6 bg-custom-gray py-20 px-7">
         <div className="flex flex-col text-left md:w-1/2 w-full gap-4">
-          <h2 className="font-bold lg:text-[40px] md:text-[30px] text-[24px] w-full font-[Euclid Circular A] text-black">
+          <motion.h2
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{
+              delay: 0.2,
+              x: { type: "spring", stiffness: 60 },
+              opacity: { duration: 1 },
+              ease: "easeIn",
+            }}
+            className="font-bold lg:text-[40px] md:text-[30px] text-[24px] w-full font-[Euclid Circular A] text-black"
+          >
             Your Path to Easier Grocery Management and Shopping
-          </h2>
+          </motion.h2>
           {lists.map((list, i) => (
-            <div key={i} className="flex flex-row items-center gap-2">
+            <motion.div
+              key={i}
+              className="flex flex-row items-center gap-2"
+              initial={{ x: -100, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{
+                delay: 0.4,
+                x: { type: "spring", stiffness: 60 },
+                opacity: { duration: 1 },
+                ease: "easeIn",
+              }}
+            >
               <p className="  text-orange-400">{list.icon}</p>
               <h6 className="font-medium md:text-[18px] text-[9px]">
                 {list.title}
               </h6>
-            </div>
+            </motion.div>
           ))}
         </div>
 
-        <div className="">
+        <motion.div
+          className=""
+          initial={{ x: 100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{
+            delay: 0.2,
+            x: { type: "spring", stiffness: 60 },
+            opacity: { duration: 1 },
+            ease: "easeIn",
+          }}
+        >
           <Image
             src={Features4}
             alt="features_image"
             className="md:w-[360px] w-[343px]"
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
